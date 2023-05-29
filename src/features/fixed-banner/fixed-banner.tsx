44a1258a4f5187889e4d24155d62f8d1e5596357
gabrielpasquantonio <gabriel.pasquantonio@xometry.com>
2023-05-29 09:43:50 -0300

    feat: create flex component

--- src/features/fixed-banner/fixed-banner.tsx
+++ src/features/fixed-banner/fixed-banner.tsx
@@ -1,13 +1,20 @@
 import { FixedBannerContainer } from './fixed-banner.styles';
 import useIsMobile from '../../hooks/is-mobile';
+import Text from '../../components/text';
+import Flex from '../../components/flex';
 
 function FixedBanner() {
-	const isMobile = useIsMobile('mobileLandscape');
+	const isMobile = useIsMobile();
 
 	return (
 		<FixedBannerContainer isMobile={isMobile}>
-			Não limite sua criatividade, junte-se a familia Blocks por apenas
-			BRL 19,99
+			<Flex gap="18px">
+				<Text isMobile={isMobile} kind="label">
+					Não limite sua criatividade, junte-se a familia Blocks por
+					apenas <b>BRL 19,99</b>
+				</Text>
+				<button type="submit">a</button>
+			</Flex>
 		</FixedBannerContainer>
 	);
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,55 @@
+// Theme specification for styled-system
+
+// see https://theme-ui.com/theme-spec/
+// and https://github.com/system-ui/theme-specification/issues/8#issuecomment-582705042
+import {
+	BackgroundProps,
+	BorderProps,
+	ColorProps,
+	FlexboxProps,
+	LayoutProps,
+	PositionProps,
+	ShadowProps,
+	SpaceProps,
+	TypographyProps,
+	background,
+	border,
+	color,
+	compose,
+	flexbox,
+	layout,
+	position,
+	shadow,
+	space,
+	typography,
+} from 'styled-system';
+
+const StyledSystemStyles = compose(
+	background,
+	border,
+	color,
+	flexbox,
+	layout,
+	position,
+	shadow,
+	typography,
+	space,
+);
+
+// We should use Intersection Types instead of Union Types to create a new Type with all available properties together instead
+// of just provide common properties.
+// https://www.typescriptlang.org/docs/handbook/unions-and-intersections.html#unions-with-common-fields
+// https://www.typescriptlang.org/docs/handbook/unions-and-intersections.html#intersection-types
+
+type StyledSystemPropsDef = BackgroundProps &
+	BorderProps &
+	ColorProps &
+	FlexboxProps &
+	LayoutProps &
+	PositionProps &
+	ShadowProps &
+	SpaceProps &
+	TypographyProps;
 const breakpoints = ['576px', '768px', '1024px', '1280px'];
 const fontWeights = {
 	light: 200,
@@ -21,13 +73,30 @@ const lineHeights = [
 	'22px', // 3
 	'25px', // 4
 ];
+const spaces = [
+	'0px', // 0
+	'4px', // 1
+	'8px', // 2
+	'12px', // 3
+	'16px', // 4
+	'20px', // 5
+	'24px', // 6
+	'28px', // 7
+	'32px', // 8
+	'36px', // 9
+	'40px', // 10
+];
 const theme = {
 	breakpoints,
 	fontWeights,
 	fontSizes,
 	lineHeights,
+	space: spaces,
 };
 
-export { theme };
+// This is a way of getting around the isolated modules flag that is set by default.
+export type StyledSystemProps = StyledSystemPropsDef;
+
+export { StyledSystemStyles, theme };
 
 export default theme;
