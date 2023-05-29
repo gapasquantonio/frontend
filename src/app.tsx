8d173d2429babb3df9ae42456987f81f2db8d8b4
gabrielpasquantonio <gabriel.pasquantonio@xometry.com>
2023-05-29 04:04:46 -0300

    feat: wip

---
+++ src/app.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+	Route,
+	Routes,
+	unstable_HistoryRouter as HistoryRouter,
+} from 'react-router-dom';
+import { css, Global } from '@emotion/react';
+import { Helmet } from 'react-helmet';
+import history from './history';
+
+const globalStyles = css`
+	html,
+	body,
+	#root,
+	#main {
+		height: 100%;
+	}
+`;
+
+const fullHeightAndWidth = {
+	width: '100vw',
+	height: '100vh',
+};
+
+export default function App() {
+	const { width, ref } = useResizeDetector({ handleHeight: false });
+
+	return (
+		<div className="App">
+			<HistoryRouter history={history}>
+				<Routes>
+					<Route
+						path="*"
+						element={
+							<>
+								<Global styles={globalStyles} />
+								<Helmet
+									defaultTitle="Blocks"
+									titleTemplate="%s | Block"
+								/>
+								<div css={fullHeightAndWidth} ref={ref}>
+									<PagesCasted />
+								</div>
+							</>
+						}
+					/>
+				</Routes>
+			</HistoryRouter>
+		</div>
+	);
+}
