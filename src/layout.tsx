4f0a9f09ce618f89fcea71df4f88bfb9a48d9416
gabrielpasquantonio <gabriel.pasquantonio@xometry.com>
2023-05-30 07:50:32 -0300

    fix: vh for mobile and tablet views

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,36 +1,58 @@
 import { Outlet } from 'react-router-dom';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import TopNavigation from './features/top-navigation';
 import useIsMobile from './hooks/is-mobile';
 import Footer from './features/footer';
 
 export type TopNavContainerProps = {
-	isMobile: boolean;
+	headerAndFooterHeight: number;
 };
 const PageContainer = styled.div<TopNavContainerProps>`
 	background-color: #fbfbfb;
-	height: calc(100vh - ${({ isMobile }) => (isMobile ? '68' : '212')}px);
+	height: calc(
+		100vh - ${({ headerAndFooterHeight }) => headerAndFooterHeight}px
+	);
 `;
 
 const ContentContainer = styled.div<TopNavContainerProps>`
 	display: flex;
-
 	margin: auto;
-	height: calc(100vh - ${({ isMobile }) => (isMobile ? '68' : '212')}px);
+	height: calc(
+		100vh - ${({ headerAndFooterHeight }) => headerAndFooterHeight}px
+	);
 `;
 
 export default function Layout() {
-	const isMobile = useIsMobile('mobileLandscape');
+	const isMobile = useIsMobile();
+	const [isBannerClosed, setBannerClosed] = useState<boolean>(false);
+
+	const getFooterAndHeaderTotalHeigth = (): number => {
+		if (!isMobile) {
+			return 212;
+		}
+		if (!isBannerClosed) {
+			return 438;
+		}
+		return 252;
+	};
 
 	return (
 		<>
 			<TopNavigation />
-			<PageContainer isMobile={isMobile}>
-				<ContentContainer isMobile={isMobile}>
+			<PageContainer
+				headerAndFooterHeight={getFooterAndHeaderTotalHeigth()}
+			>
+				<ContentContainer
+					headerAndFooterHeight={getFooterAndHeaderTotalHeigth()}
+				>
 					<Outlet />
 				</ContentContainer>
 			</PageContainer>
-			<Footer />
+			<Footer
+				setBannerClosed={setBannerClosed}
+				isBannerClosed={isBannerClosed}
+			/>
 		</>
 	);
 }
